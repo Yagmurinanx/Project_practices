a4f245d31fba4d056ab81c4bbf206cbfccb00c64
Yağmur İnan <ygmrinan00@gmail.com>
2024-01-22 15:50:36 +0300

    message

--- Weather App/index.js
+++ Weather App/index.js
@@ -1,7 +1,7 @@
 const weatherForm = document.querySelector(".weatherForm");
 const cityInput = document.querySelector(".cityInput");
 const card = document.querySelector(".card");
-const apiKey = "77e14c2776adef163b515d6c78ddcbb2"
+const apiKey = process.env.API_KEY;
 
 weatherForm.addEventListener("submit", async event => {
 
